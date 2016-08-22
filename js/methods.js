cfd8c431540dfe10c5baa41b9f853ca80c6be879
blessedsoy <unmigong@gmail.com>
2016-08-22 12:25:19 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function fancySchmancy(){
+  $('p#bacon').addClass('iAmFancy');
+  return $('.iAmFancy');
+}
+
+function addText(str){
+  $('p').eq(1).append(str)
+  return $('p').eq(1);
+}
+
+function showDolphin(){
+  $('img#hidden').show(); 
+}
+
+function hideImage(){
+  $('img[alt="pie in face"]').hide(); 
+}
+
+function fadeIt(){
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  $('p').eq(1).fadeOut(0);
+}
+
+function findItByClass(str){
+  return $(str).hasClass('flatironLink');
+}
+
+function formValue(){
+  return $('input:last').val();
+}
