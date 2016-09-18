a3ce7f69a6d2eecb04d2ff49ec064e11d257f2f2
aehmt <calisahmet@gmail.com>
2016-09-18 17:01:52 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,39 @@
 'use strict';
+
+function fancySchmancy() {
+  return $("p").addClass("iAmFancy");
+}
+
+function addText(text) {
+  return $("p:first-child + p").append(text);
+  // return $("p#bacon").append(text);
+}
+
+function showDolphin() {
+  return $("#hidden").show();
+}
+
+function hideImage() {
+  return $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  return $('#favorite-snacks').fadeIn().css("display: block; opacity: 0.0;");
+}
+
+function fadeItOut() {
+  return $("p:first-child + p").fadeOut(0).css("display: none;");
+}
+
+function findItByClass(tag) {
+  if ($(tag).hasClass('flatironLink')) {
+    return true;
+  } else {
+    return false;
+  } 
+}
+
+function formValue() {
+  return $('input:last').val()
+}
+
