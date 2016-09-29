86e11bf5067fa131c7ce3b46218d83360e9a8eba
Derick Quartey <niikwartey1@gmail.com>
2016-09-29 17:43:26 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(someText) {
+  return $('p:nth-child(2)').append(someText);
+}
+
+function showDolphin() {
+  return $('img#hidden').show()
+}
+
+function hideImage() {
+  return $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt() {
+  return $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  return $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(s) {
+  return $(s).hasClass("flatironLink");
+}
+
+function formValue() {
+  return $('form input').last().val();
+}
