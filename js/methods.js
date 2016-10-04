7f5e858ad33cd0e226243d090749538b86ca4102
satub <satu.barnhill@gmail.com>
2016-10-04 11:10:07 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy(){
+  return $('p#bacon').addClass("iAmFancy");
+}
+
+function addText(addThis){
+  return $('p:nth-child(2)').append(addThis);
+}
+
+function showDolphin(){
+  $('img#hidden').show();
+}
+
+function hideImage(){
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt(){
+  $('#favorite-snacks').fadeIn();
+}
+
+function fadeItOut(){
+  return $('p:nth-child(2)').fadeOut(0);
+}
+
+function findItByClass(selectThis){
+  return $(selectThis).hasClass("flatironLink");
+}
+
+function formValue(){
+  return $('input:nth-child(2)').val();
+}
