57067fa19fa2b0188a7db8fea51edb79e68c7740
fidelscodes <fidelscodes@gmail.com>
2016-10-17 16:34:22 -0400

    Done

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
-'use strict';
+// 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy');
+}
+
+function addText(string) {
+  return $('p:eq(1)').append(string);
+}
+
+function showDolphin() {
+  $('img#hidden').show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $('div#favorite-snacks').fadeIn();
+}
+
+function fadeItOut() {
+  $('p:eq(1)').fadeOut(0);
+}
+
+function findItByClass(jQuerySelector) {
+  return $(jQuerySelector).hasClass('flatironLink');
+}
+
+function formValue() {
+  var input = $("input[type='checkbox']").last();
+  return input.val();
+}
