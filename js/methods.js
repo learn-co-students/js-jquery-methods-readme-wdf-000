854a80d016e7f96f305610b1bf479c908a84c96c
paige11 <ellis.paige.v@gmail.com>
2016-10-21 23:19:42 -0400

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function fancySchmancy() {
+  return $('p#bacon').addClass('iAmFancy')
+}
+
+function addText(string) {
+  return $('p:nth-child(2)').append(string)
+}
+
+function showDolphin() {
+  $('img#hidden').show()
+}
+
+function hideImage() {
+  $('img[alt="pie in face"]').hide()
+}
+
+function fadeIt() {
+  $('#favorite-snacks').fadeIn(2000)
+}
+
+function fadeItOut() {
+  $('p:nth-child(2)').fadeOut(0)
+}
+
+function findItByClass(select) {
+  return $(select).hasClass('flatironLink')
+}
+
+function formValue() {
+  return $('input:nth-child(2)').val()
+}
