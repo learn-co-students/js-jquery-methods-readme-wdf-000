e5255f2d1e63e9d9345f1dbcdb7cf76e143f828b
sami4thewin <samiomarmohammed@gmail.com>
2016-11-07 11:59:34 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function fancySchmancy() {
+  return $("p#bacon").addClass("iAmFancy");
+}
+
+function addText(string) {
+  // return $($("p")[1]).append(string);
+  return $("p:nth-child(2)").append(string);
+}
+
+function showDolphin() {
+  $("img#hidden").show();
+}
+
+function hideImage() {
+  $("img[alt='pie in face']").hide();
+}
+
+function fadeIt() {
+  $("div#favorite-snacks").fadeIn();
+}
+
+function fadeItOut() {
+  $("p:nth-child(2)").fadeOut(0);
+}
+
+function findItByClass(selector) {
+  return $(selector).hasClass('flatironLink');
+}
+
+function formValue() {
+  return $("input:last").val();
+}
