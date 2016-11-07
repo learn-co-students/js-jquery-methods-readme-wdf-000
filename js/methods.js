7df78e32b7d462deb4f57366c15fff5a3d7692dc
Isuru <github email address>
2016-11-07 10:26:10 -0500

    Done.

--- js/methods.js
+++ js/methods.js
@@ -1,1 +1,36 @@
 'use strict';
+function fancySchmancy(){
+  return $('p#bacon')[0];
+}
+
+function addText(input){
+  return $('p#bacon').append(input)
+}
+
+function showDolphin(){
+  return $('img#hidden').show();
+}
+
+function hideImage(){
+  return $('img[alt="pie in face"]').hide();
+}
+
+function fadeIt(){
+  return $('div#favorite-snacks').html();
+}
+
+function fadeItOut(){
+  return $('p:first-child').next().html();
+}
+
+function findItByClass(tag){
+  if (tag === 'a'){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function formValue(){
+  return $('input:last-child').val();
+}
